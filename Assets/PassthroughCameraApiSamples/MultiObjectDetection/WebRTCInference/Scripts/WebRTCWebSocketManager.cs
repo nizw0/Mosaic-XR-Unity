0360ac204deaa27fe6eb29539822341545e63fbb
nizw0 <me@nizw0.dev>
2025-08-27 17:15:54 +0800

    chore: add websocket manager files

---
+++ Assets/PassthroughCameraApiSamples/MultiObjectDetection/WebRTCInference/Scripts/WebRTCWebSocketManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace PassthroughCameraSamples.MultiObjectDetection
+{
+    // TODO: Implement WebRTC signaling by WebSocket
+    public class WebRTCWebSocketManager : MonoBehaviour
+    {
+        #region Unity Functions
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+
+        }
+        #endregion
+    }
+}
